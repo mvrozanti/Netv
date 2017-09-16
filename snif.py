a5ad1d258b01f9054e2486cbf7d6706f40df022c
mvrozanti <mvrozanti@hotmail.com>
2017-09-16 15:22:05 -0300

    Created README and makes use of pixels instead of columns

--- snif.py
+++ snif.py
@@ -21,7 +21,12 @@ HEIGHT=600
 
 if os.name == 'nt':
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-    
+
+def on_close():
+    # if we’re using Windows, turn off promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
 def tuple_to_color(rgb):
     color = '#'
     r,g,b=rgb[0],rgb[1],rgb[2]
@@ -33,40 +38,51 @@ def tuple_to_color(rgb):
 t = Tk()
 c = Canvas(t, width=WIDTH, height=HEIGHT, bg='black')
 c.pack()
-#img = PhotoImage(width=WIDTH, height=HEIGHT)
-#c.create_image((WIDTH/2, HEIGHT/2), image=img, state='normal')
-#pixels = []
-canvas_columns = []
-x, y=1,1
+pixels = []
+x, y = 0, 0
 def draw():
     bs = sniffer.recvfrom(65565)
     les_bytes = bs[0]
     rgb = []
-    global canvas_columns
+    global pixels
     global x
     global y
     for i in range(len(les_bytes)):
         rgb.append(les_bytes[i] % 255)
         if len(rgb) == 3:
             color = tuple_to_color(tuple(rgb))
-            col = c.create_line(x, 0, x, HEIGHT, fill=color, width=1)
-            canvas_columns.append(col)
-            if len(canvas_columns) > WIDTH: 
-                c.delete(canvas_columns[0])
-                canvas_columns.pop(0)
-            # move over shifting * to the right
-            for l in canvas_columns:
-                xy = c.coords(l)
-                xy[0]+=1
-                xy[2]+=1
-                c.coords(l, xy)
-            rgb.clear()       
-            c.update_idletasks()
-            c.update()
+            rgb.clear()
+            pixel = c.create_line((x+3, y+3), (x+4, y+4), fill=color, width=10)
+            if len(pixels) <= x:
+                pixels.insert(x, [])
+            pixels[x].insert(y+1, pixel)
+            #y+=1
+            #if y > HEIGHT:
+            #    y = 0
+            #    x += 1
+            #    if x > WIDTH:
+            #        x = y = 0
+            # shift *
+            for row in pixels:
+                for p in row:
+                    xy = c.coords(p)
+                    p_width = float(c.itemcget(p, 'width'))
+                    xy[1] = xy[1] + p_width#y++
+                    xy[3] = xy[3] + p_width
+                    if xy[1] > HEIGHT:
+                        xy[1] = 0#y=0
+                        xy[3] = 1
+                        xy[0] = xy[0] + p_width#x++
+                        xy[2] = xy[2] + p_width
+                    if xy[0] > WIDTH:
+                        xy[0] = 0#x=y=0
+                        xy[2] = 1
+                        xy[1] = 0
+                        xy[3] = 1
+                    c.coords(p, xy)
+                    c.update_idletasks()
+                    c.update()
     c.after(1, draw)
 draw()
+t.protocol('WM_DELETE_WINDOW', on_close)
 t.mainloop()
-
-# if we’re using Windows, turn off promiscuous mode
-if os.name == 'nt':
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
